5b1d8ccd183f3644aab31ad89f01e952f796b8b2
marceloclribeiro <54719631+marceloclribeiro@users.noreply.github.com>
2022-11-08 16:01:39 -0300

    Update mpirun.sh

--- mpirun.sh
+++ mpirun.sh
@@ -9,7 +9,7 @@ then
     # If first parameter is not empty, use clang to compile the file
     if [ $# -eq 0 ]; 
     then
-        mpicc -fopenmp -Wall  main.c -pthread -O3 -o main
+        mpicc --host localhost:4 -fopenmp -Wall  main.c -pthread -O3 -o main
     else
         /usr/local/opt/llvm/bin/clang -fopenmp -L/usr/local/opt/llvm/lib main.c -o main
     fi
